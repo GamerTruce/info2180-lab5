d0ab2605ef87d73060ffd7fe53170a0e2dc61ab3
GamerTruce <maleikmiller@gmail.com>
2021-11-09 23:09:28 -0500

    Now Accept GET request variables

--- world.php
+++ world.php
@@ -5,9 +5,24 @@ $password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+if(isset($_GET)){
+  $country = $_GET['country'];
+  $context =$_GET['context'];
+
+if($context=="cities"){
+ 
+      $stmt = $conn->query("SELECT cities.name, cities.district, cities.population FROM cities JOIN countries ON cities.country_code = countries.code WHERE countries.name LIKE \"%$country%\" ORDER BY countries.name ASC");
+      $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+else
+
+{
+   $countrieswhere = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+   $outcome= $countrieswhere->fetchAll(PDO::FETCH_ASSOC);
+}
+}
 
 ?>
 <ul>
