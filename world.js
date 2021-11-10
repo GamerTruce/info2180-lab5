fd7525cbbdeb29ee2d07281a5356d0ee4c035e64
GamerTruce <maleikmiller@gmail.com>
2021-11-09 23:25:22 -0500

    Added Event Listener

---
+++ world.js
@@ -0,0 +1,16 @@
+window.onload = function(){
+    var lookup_button = document.getElementById("lookup");
+    var city_button = document.getElementById("lookupCities");
+
+    lookup_button.addEventListener("click",find);
+    city_button.addEventListener("click",findCity);
+}
+
+
+function find() {
+
+}
+
+function findCity() {
+
+}
